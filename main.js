48b237c40e63ebcd22c4ba9e486a3c84dd21626e
saim514 <81713694+saim514@users.noreply.github.com>
2022-03-14 17:45:45 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ function preload()
 
 function setup()
 {
-    canvas = createCanvas(300,300);
+    canvas = createCanvas(400,400);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(300,300);
+    video.size(400,400);
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
@@ -20,7 +20,7 @@ function setup()
 
 function draw()
 {
- image(video, 0, 0, 300, 300);
+ image(video, 0, 0, 400, 400);
 }
 
 function gotPoses(results)
